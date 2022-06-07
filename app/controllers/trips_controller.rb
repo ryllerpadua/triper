6338f0e89ee130ac3158f8c671c8d66c214fc718
ryllerpadua <ryllerpadua@yahoo.com.br>
2022-06-07 15:33:24 -0300

    new e create trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,16 @@
 class TripsController < ApplicationController
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.save
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:date)
+  end
 end
