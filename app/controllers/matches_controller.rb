15ef1ac7d31377e83db7f1a3d5206181d6122375
Nathalia Vale <nat_sv2@icloud.com>
2022-06-08 16:11:20 -0300

    matches-controller-ok

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,15 +1,23 @@
 class MatchesController < ApplicationController
 
   def index
-
+    @matches = Match.all
   end
 
 
-  def new
+  # def new
 
-  end
+  # end
 
   def create
+    trip1 = Trip.find(params[:trip_id])
+    trip2 = current_user.trips.find_by(project: trip1.project, date: trip1.date)
+    match = Match.new(trip1: trip1, trip2: trip2)
+    if match.save
+      redirect_to matches_path
+    else
+      redirect_to trips_path, notice: "Nao foi possível criar um match =("
+    end
   end
 
 end
