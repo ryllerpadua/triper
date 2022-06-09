e8f2b73f71049455435aaf9aef366a4c9e7c7592
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-06-09 12:44:25 -0300

    Arrumado todos chats vendo mensagens individuais

--- app/channels/match_channel.rb
+++ app/channels/match_channel.rb
@@ -2,7 +2,7 @@ class MatchChannel < ApplicationCable::Channel
   def subscribed
     # Gets called as soon as there is a subscription made from a client
 
-    stream_from "everyone"
+    stream_from "match-#{params[:id]}"
   end
 
   def unsubscribed
--- app/javascript/controllers/match_subscription_controller.js
+++ app/javascript/controllers/match_subscription_controller.js
@@ -2,8 +2,11 @@ import { Controller } from "stimulus"
 import consumer from '../channels/consumer'
 
 export default class extends Controller {
+  static values = { matchId: Number }
   connect() {
-   consumer.subscriptions.create("MatchChannel", {received: (message) => {
+   consumer.subscriptions.create(
+    { channel: "MatchChannel", id: this.matchIdValue },
+    {received: (message) => {
     this.element.insertAdjacentHTML('beforeend', message)
    } }
    )
