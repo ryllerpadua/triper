0d7ad9abb2a15190a92f09896898cf7dbc6e0f05
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-06-09 17:14:11 -0300

    Limitado a 10 trips por usuário

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ApplicationRecord
   belongs_to :user
+  validates_associated :user
   belongs_to :project
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :trips, dependent: :destroy
+  validates_length_of :trips, maximum: 9
 
 
   devise :database_authenticatable, :registerable,
