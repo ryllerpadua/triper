12ba227ba48802f7190b287b10ae46b4959ed543
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-06-09 13:00:56 -0300

    Código arrumado conforme video aula

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     @message.match = @match
     @message.user = current_user
     if @message.save
-      ActionCable.server.broadcast("match-#{@match.id}",
+      MatchChannel.broadcast_to(@match,
         render_to_string(partial: 'messages/message', locals: { message: @message })
       )
       redirect_to match_path(@match, anchor: "message-#{@message.id}")
