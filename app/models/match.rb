f2f79e4927aac61b41af9163f72ff1d3c92d530b
ryllerpadua <ryllerpadua@yahoo.com.br>
2022-06-16 09:53:51 -0300

    foto notification

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,11 @@ class Match < ApplicationRecord
   has_many :notifications, dependent: :destroy
   belongs_to :trip1, class_name: 'Trip'
   belongs_to :trip2, class_name: 'Trip'
+  def other_user(user)
+    if user == trip1.user
+      return trip2.user
+    else
+      return trip1.user
+    end
+  end
 end
