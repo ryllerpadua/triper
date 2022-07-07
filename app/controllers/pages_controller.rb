cf18bcf852073fa46cbbf6d9c88ae216cfcc40bf
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-07-07 17:29:14 -0300

    Busca funcionando novamente

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
-  def home
-  end
-
   def dashboard
     @countries = current_user.projects.pluck(:country).uniq
-    @trips = current_user.trips.order(:project_id)
+    @trips = Trip.all.where.not(user: current_user)
+    @usertrips = current_user.trips.order(:project_id)
+
     projects = current_user.trips.map { |trip| trip.project }
-    @users_search = Trip.where(project: projects).where.not(user: current_user)
-    @users_search.select do |trip|
-      interval = trip.date.beginning_of_month..trip.date.end_of_month
-      current_user.trips.where(project: trip.project, date: interval).count.positive?
-    end
+    interval = current_user.trips.map { |trip| trip.date.beginning_of_month..trip.date.end_of_month}
+    @users_search = Trip.where(project: projects).and(Trip.where(date: interval)).where.not(user: current_user)
     @users_search = @users_search.reject { |trip| Match.where(trip1: trip).count.positive? || Match.where(trip2: trip).count.positive? }
 
     @matches = Match.where(trip1_id: current_user.trips.ids).or(Match.where(trip2_id: current_user.trips.ids))
