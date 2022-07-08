6f302e049ba0d49433f5c39b483a1793df55cdfa
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-07-08 00:23:37 -0300

    Select ao invés do each no dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,8 @@ class PagesController < ApplicationController
     @usertrips = current_user.trips.order(:project_id)
 
     projects = current_user.trips.map { |trip| trip.project }
-    interval = current_user.trips.map { |trip| trip.date.beginning_of_month..trip.date.end_of_month}
-    @users_search = Trip.where(project: projects).and(Trip.where(date: interval)).where.not(user: current_user)
+    interval = current_user.trips.map { |trip| trip.date.beginning_of_year..trip.date.end_of_year}
+    @users_search = Trip.where(project: projects, date: interval).where.not(user: current_user)
     @users_search = @users_search.reject { |trip| Match.where(trip1: trip).count.positive? || Match.where(trip2: trip).count.positive? }
 
     @matches = Match.where(trip1_id: current_user.trips.ids).or(Match.where(trip2_id: current_user.trips.ids))
