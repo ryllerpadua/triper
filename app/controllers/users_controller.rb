39d871c37df08f1e5a30755364d6a582b8f02241
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-07-12 20:04:53 -0300

    listar trips criadas pelos usuários (unfinished)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  # def countries
-  #   @countries = @user.trips.projects.pluck(:country)
-  #   @trips = @user.trips.order(:project_id)
-  # end
-
 end
